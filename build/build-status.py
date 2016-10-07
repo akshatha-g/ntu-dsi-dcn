87149c596675e0f3d070bf990f199e434a0539cf
Akshatha Gangadharaiah <agangadh@ucsd.edu>
2016-10-07 02:14:41 -0700

    NTU changes for MAC

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['BCube', 'Fat-tree-AlFares', 'Fat-tree-Bilal', 'Fat-tree']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
